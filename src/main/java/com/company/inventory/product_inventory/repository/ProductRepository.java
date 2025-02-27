7539978dba9cad264e4c45408007440a44e35707
leilanyaragao <leilanyaragao26@gmail.com>
2025-02-27 03:18:41 -0300

    add repository

---
+++ src/main/java/com/company/inventory/product_inventory/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.company.inventory.product_inventory.repository;
+
+import com.company.inventory.product_inventory.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, Integer> {
+    Product findProductBySku(Integer sku);
+
+    @Query("{'inventory.warehouses.locality': ?0}")
+    List<Product> findByWarehouseLocality(String locality);
+}
